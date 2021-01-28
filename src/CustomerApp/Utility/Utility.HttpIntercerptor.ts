f0ee0a6bc6dbaf1784298f8080a475bb79cf35c3
Alex Basystyi <alexbasisty@gmail.com>
2021-01-28 11:22:53 +0100

    http interceptor utility and imports

---
+++ src/CustomerApp/Utility/Utility.HttpIntercerptor.ts
@@ -0,0 +1,3 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
