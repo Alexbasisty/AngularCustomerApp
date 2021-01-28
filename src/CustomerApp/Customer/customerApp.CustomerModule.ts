eeb9de3363a7ce390afb9f929b6dcfaeccb6767b
Alex Basystyi <alexbasisty@gmail.com>
2021-01-28 11:48:49 +0100

    httpinterceptor logic and import at Customer module

--- src/CustomerApp/Customer/customerApp.CustomerModule.ts
+++ src/CustomerApp/Customer/customerApp.CustomerModule.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { CustomerComponent } from './customerApp.CustomerComponent';
 import { CustomerRoutes } from '../Routing/CustomerApp.CustomerRouting';
 import { GridComponent } from '../Utility/CustomerApp.GridComponent';
+import { MyInterceptor } from '../Utility/Utility.HttpIntercerptor';
 
 
 @NgModule({
@@ -21,7 +22,9 @@ import { GridComponent } from '../Utility/CustomerApp.GridComponent';
     HttpClientModule,
     RouterModule.forChild(CustomerRoutes),
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi:true}
+  ],
   bootstrap: [CustomerComponent]
 })
 export class CustomerModule { }
